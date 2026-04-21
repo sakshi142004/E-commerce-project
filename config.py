6bea1570c1e4dfa4fd450d54a6919e74c0bcd0a9
sakshi bhati <bhatisakshi2004@gmail.com>
2026-04-21 18:55:36 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,4 +1,16 @@
+import os
+
+
 class Config:
     SECRET_KEY = '95c32ec77c8d69db8b76e50fab0d6a9d'
+
     SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://root:root@localhost/belt_paradise'
     SQLALCHEMY_TRACK_MODIFICATIONS = False
+
+    BASE_DIR = os.path.abspath(os.path.dirname(__file__))
+    UPLOAD_FOLDER = os.path.join(BASE_DIR, "static/images")
+
+    CLOUDINARY_CLOUD_NAME = "dhjl5plm8"
+    CLOUDINARY_API_KEY = "881392445818876"
+    CLOUDINARY_API_SECRET = "wJcwQke-UHL4YQAAskXSYDFNiTE"
+
